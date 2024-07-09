5247d29210812c2e836d452ff59d5b9d50614eb8
Checho-92 <sergio.smt92@gmail.com>
2024-07-09 13:57:23 -0500

    CRUD de notas funcional

--- my-notes-app/src/ui/components/footer.tsx
+++ my-notes-app/src/ui/components/footer.tsx
@@ -4,8 +4,8 @@ const Footer: React.FC = () => {
   return (
     <footer className="text-gray-600 body-font">
       <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-        <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-         
+        <a className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:py-2 sm:mt-0 mb-3">
+          Sergio Marin
           <span className="ml-3 text-xl"></span>
         </a>
         <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
